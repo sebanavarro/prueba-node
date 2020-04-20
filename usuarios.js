e18650ee025bea9c778e608cc329ff71f9b9fb06
Seba Navarro <sebastian.navarro87@gmail.com>
2020-04-20 12:50:23 -0300

    agregue el archivo variables.js

---
+++ usuarios.js
@@ -0,0 +1,12 @@
+//Crear una calculadora
+
+let sumar = require('./sumar')
+let restar= require('./restar')
+let multiplicar= require('./multiplicar')
+let dividir= require('./dividir')
+
+// Ejecutando el código
+console.log(multiplicar(3,2))
+console.log(dividir(3,2))
+console.log(sumar(2,2))
+console.log(restar(5,2))
